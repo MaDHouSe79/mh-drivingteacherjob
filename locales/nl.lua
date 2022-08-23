471d02221340512e430bd8b00e2d4e2cfc8817ab
MaDHouSe79 <madhouse1979@gmail.com>
2022-08-23 12:06:09 +0200

    Update nl.lua

--- locales/nl.lua
+++ locales/nl.lua
@@ -1,42 +1,36 @@
 local Translations = {
-
+	
     error = {
-		['area_is_obstructed'] = 'Error, the receiving area is obstructed by something, try again',
-		['unknow_id'] = "Ongeldige ID",
+		['area_is_obstructed'] = 'Fout, het ontvangstgebied wordt door iets belemmerd, probeer het opnieuw',
 		['unknow_id_or_type'] = "Ongeldige ID of Type",
+		['invalid_id'] = "U heeft een ongeldige ID ingevoerd..",
+		['not_a_instructor'] = "Je bent geen rijinstructeur..",
     },
 
     info = {
-		['on_duty'] = '[E] - Ga in dienst',
-        ['off_duty'] = '[E] - Ga uit dienst',
-        ['onoff_duty'] = 'In/Uit dienst',
+		['on_duty'] = '[E] - Ga in Dienst',
+        ['off_duty'] = '[E] - Ga uit Dienst',
+        ['onoff_duty'] = 'In/Uit Dienst',
     },
 
 	notify = {
-		['student_not_found'] = "Geen student gevonden",
-		['no_info_found'] = "Geen test info gevonden"
+		['student_not_found'] = "Geen leerling gevonden",
+		['no_info_found'] = "Geen rijexameninformatie gevonden",
+		['granted_access_license'] = "Speler met ID %{id} heeft toegang gekregen tot %{license} licentie",
+		['passed_for_license'] = "Je bent gelaagd voor %{license} licentie"
+		['your_license_has_been_taken'] = "Uw rijbewijs is ingenomen door %{player}",
+		['license_has_been_taken'] = "Speler met ID %{id} heeft geen geldig licentie meer",
 	},
 
 	menu = {
-		["customer_id"] = "Speler ID",
-		['main_header'] = "RijSchool Menu",
-		['main_description'] = "Start een rijtest met een speler",
-		['give_description'] = "Rijbewijs Geven",
-		['take_description'] = "Rijbewijs Intrekken",
-		['licence_types'] = "N/A/AM/B/BE/C/CE/D/DE/P/T/B",
-		
-		['driving_theory_test'] = 'Theorie examen[€%{price}]',
-		['am_class_driving_practice_test'] = 'AM (Brommer) [€%{price}]',
-		['a_class_driving_practice_test']  = 'A (Motor) [€%{price}]',
-		['b_class_driving_practice_test']  = 'B (Auto) [€%{price}]',
-		['be_class_driving_practice_test'] = 'BE (Auto + Aanhanger) [€%{price}]',
-		['c_class_driving_practice_test']  = 'C (Vrachwagen) [€%{price}]',
-		['ce_class_driving_practice_test'] = 'CE (Vrachwagen + Aanhanger) [€%{price}]',
-		['d_class_driving_practice_test']  = 'D (Bus) [€%{price}]',
-		['de_class_driving_practice_test'] = 'DE (Bus + Aanhanger) [€%{price}]',
-		['p_class_driving_practice_test']  = 'P (Vliegbrevet) [€%{price}]',
-		['t_class_driving_practice_test']  = 'T (Boat) [€%{price}]',
-		['h_class_driving_practice_test']  = 'H (Vliegbrevet) [€%{price}]',
+		["customer_id"] = "Player ID",
+		['main_header'] = "Rijschool Menu",
+		['licence_types'] = "Types N/A/AM/B/BE/C/CE/D/DE/T/H/P",
+	},
+
+	command = {
+		['add_help'] = "Een licentie geven ",
+		['remove_help'] = "Een licentie afnemen",
 	}
 }
 
