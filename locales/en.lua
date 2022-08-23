0e79715981840598f87075c67e5b788f27eeefce
MaDHouSe79 <madhouse1979@gmail.com>
2022-08-23 12:05:52 +0200

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -2,40 +2,35 @@ local Translations = {
 	
     error = {
 		['area_is_obstructed'] = 'Error, the receiving area is obstructed by something, try again',
-		['unknow_id'] = "Ongeldige ID",
-		['unknow_id_or_type'] = "Ongeldige ID of Type",
+		['unknow_id_or_type'] = "Invalid ID or Type",
+		['invalid_id'] = "You have entered a invalid ID..",
+		['not_a_instructor'] = "You are not a driving instructor..",
     },
 
     info = {
-		['give_keys_to_student'] = "Geef de sleutels aan je leerling, en begin de rijexamen.",
-		['on_duty'] = '[~g~E~s~] - Ga in dienst',
-        ['off_duty'] = '[~r~E~s~] - Ga uit dienst',
-        ['onoff_duty'] = '~g~In~s~/~r~Uit~s~ dienst',
+		['on_duty'] = '[E] - Go into service',
+        ['off_duty'] = '[E] - Get out of service',
+        ['onoff_duty'] = 'In/Out service',
     },
 
 	notify = {
 		['student_not_found'] = "No student found",
-		['no_info_found'] = "No driving test info found"
+		['no_info_found'] = "No driving test info found",
+		['granted_access_license'] = "Player with ID %{id} has been granted access to %{license} license",
+		['passed_for_license'] = "You passed your %{license} license"
+		['your_license_has_been_taken'] = "Your driver's license has been taken away by %{player}",
+		['license_has_been_taken'] = "Player with ID %{id} no longer has a license",
 	},
 
 	menu = {
-		["customer_id"] = "Speler ID",
-		['main_header'] = "RijSchool Menu",
-		['main_description'] = "Start een rijtest met een speler",
-		['give_description'] = "Rijbewijs Geven",
-		['take_description'] = "Rijbewijs Intrekken",
-		['licence_types'] = "N/A/AM/B/BE/C/CE/D/DE/P",
-		
-		['driving_theory_test'] = 'Theorie examen[€%{price}]',
-		['am_class_driving_practice_test'] = 'AM (Brommer) [€%{price}]',
-		['a_class_driving_practice_test']  = 'A (Motor) [€%{price}]',
-		['b_class_driving_practice_test']  = 'B (Auto) [€%{price}]',
-		['be_class_driving_practice_test'] = 'BE (Auto + Aanhanger) [€%{price}]',
-		['c_class_driving_practice_test']  = 'C (Vrachwagen) [€%{price}]',
-		['ce_class_driving_practice_test'] = 'CE (Vrachwagen + Aanhanger) [€%{price}]',
-		['d_class_driving_practice_test']  = 'D (Bus) [€%{price}]',
-		['de_class_driving_practice_test'] = 'DE (Bus + Aanhanger) [€%{price}]',
-		['p_class_driving_practice_test']  = 'P (Vliegbrevet) [€%{price}]',
+		["customer_id"] = "Player ID",
+		['main_header'] = "DriveSchool Menu",
+		['licence_types'] = "Types N/A/AM/B/BE/C/CE/D/DE/T/H/P",
+	},
+
+	command = {
+		['add_help'] = "Give a license to a player",
+		['remove_help'] = "Taking a license",
 	}
 }
 
