9e51eac9a258fe394e33a3a730c2204aad06fb11
MaDHouSe79 <madhouse1979@gmail.com>
2023-08-01 13:45:52 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -28,20 +28,20 @@ server_scripts {
 
 
 files {
-    'data/**/*.meta',
-    'data/**/*.xml',
-    'data/**/*.dat',
-    'data/**/*.ytyp',    
-    'data/**/*.cs',
+    'data/*.meta',
+    'data/*.xml',
+    'data/*.dat',
+    'data/*.ytyp',    
+    'data/*.cs',
 }
 
-data_file 'HANDLING_FILE'            'data/**/handling*.meta'
-data_file 'VEHICLE_LAYOUTS_FILE'    'data/**/vehiclelayouts*.meta'
-data_file 'VEHICLE_METADATA_FILE'    'data/**/vehicles*.meta'
-data_file 'CARCOLS_FILE'            'data/**/carcols*.meta'
-data_file 'VEHICLE_VARIATION_FILE'    'data/**/carvariations*.meta'
-data_file 'CONTENT_UNLOCKING_META_FILE' 'data/**/*unlocks.meta'
-data_file 'PTFXASSETINFO_FILE' 'data/**/ptfxassetinfo.meta'
+data_file 'HANDLING_FILE'            'data/handling*.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'    'data/vehiclelayouts*.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/vehicles*.meta'
+data_file 'CARCOLS_FILE'            'data/carcols*.meta'
+data_file 'VEHICLE_VARIATION_FILE'    'data/carvariations*.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'data/unlocks.meta'
+data_file 'PTFXASSETINFO_FILE' 'data/ptfxassetinfo.meta'
 
 this_is_a_map 'yes'
 
