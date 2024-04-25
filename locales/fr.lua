d6ba179917d0cff41d92fdbcd6ce5293ec252d08
MaDHouSe <madhouse1979@gmail.com>
2024-04-25 14:51:05 +0200

    Update fr.lua

--- locales/fr.lua
+++ locales/fr.lua
@@ -4,7 +4,7 @@ local Translations = {
         ['area_is_obstructed'] = 'Erreur, la zone de réception est obstruée par quelque chose, réessayez',
         ['unknow_id_or_type'] = "ID ou type invalide",
         ['invalid_id'] = "Vous avez saisi un ID invalide.",
-        ['not_a_instructor'] = "Vous n'êtes pas un instructeur de conduite."
+        ['not_a_instructor'] = "Vous n\'êtes pas un instructeur de conduite."
     },
 
     info = {
@@ -16,16 +16,16 @@ local Translations = {
     notify = {
         ['student_not_found'] = "Aucun élève trouvé",
         ['no_info_found'] = "Aucune information sur l'examen de conduite trouvée",
-        ['granted_access_license'] = "Le joueur avec l'ID %{id} a obtenu l'accès à la licence %{license}",
+        ['granted_access_license'] = "Le joueur avec l'ID %{id} a obtenu l\'accès à la licence %{license}",
         ['passed_for_license'] = "Vous avez réussi votre permis de conduire %{license}",
         ['your_license_has_been_taken'] = "Votre permis de conduire a été retiré par %{player}",
-        ['license_has_been_taken'] = "Le joueur avec l'ID %{id} n'a plus de permis de conduire",
+        ['license_has_been_taken'] = "Le joueur avec l\'ID %{id} n\'a plus de permis de conduire",
         ['wrong_vehicle_to_park'] = "Ce véhicule ne peut pas être garé ici car ce n'est pas une voiture d'auto-école."
     },
 
     menu = {
         ["customer_id"] = "ID du joueur",
-        ['main_header'] = "Menu de l'auto-école",
+        ['main_header'] = "Menu de l\'auto-école",
         ['licence_types'] = "Types N/A/AM/B/BE/C/CE/D/DE/T/H/P",
         ['park_vehicle'] = "[E] - Garer le véhicule",
         ['open_garage'] = "[E] - Ouvrir le garage",
@@ -48,7 +48,7 @@ local Translations = {
         ['licence_h'] = "Licence de pilote d'hélicoptère",
         ['licence_p'] = "Licence de pilote d'avion",
         ['licence_r'] = "Licence de course",
-        ['licence_amb'] = "Permis d'ambulancier",
+        ['licence_amb'] = "Permis d\'ambulancier",
         ['licence_pol'] = "Permis de police"
     },
     command = {
